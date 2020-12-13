7856d9f5e5608b86b05574a13c42e7c3f1d0cd71
Sahil Rizvi <sahil.rizvi@naukri.com>
2020-12-14 03:09:29 +0530

    doing changes for debounce

--- src/main/java/practice/DayFiftyOne.java
+++ src/main/java/practice/DayFiftyOne.java
@@ -8,45 +8,53 @@ public class DayFiftyOne {
     private final ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
 
     public void debounce(Callable<Integer> c, String k, long delay){
+
+        System.out.println("keyset"+callableConcurrentHashMap.keySet());
+        System.out.println("values"+callableConcurrentHashMap.values());
         if(callableConcurrentHashMap.containsKey(k)){
             System.out.println("already contains "+k+" so remove and put new");
-            callableConcurrentHashMap.remove(k).cancel(true);
+            //callableConcurrentHashMap.remove(k).cancel(true);
+            var s = callableConcurrentHashMap.get(k);
+            if(s.isDone()){
+                System.out.println("removing "+k+" hash" +s.hashCode());
+                callableConcurrentHashMap.remove(k);
+            }
             return;
         }
 
         ScheduledFuture<?> scheduledFuture = scheduledExecutorService.schedule(c, delay,TimeUnit.MILLISECONDS);
         callableConcurrentHashMap.put(k, scheduledFuture);
-        while(!scheduledFuture.isDone()){
-            try {
-                Thread.sleep(1000);
-                System.out.println("running "+k);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        try {
-            System.out.println(scheduledFuture.get());
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }
-        callableConcurrentHashMap.remove(k);
+//        while(!scheduledFuture.isDone()){
+//            try {
+//                Thread.sleep(1000);
+//                System.out.println("running "+scheduledFuture.hashCode());
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//        try {
+//            System.out.println(scheduledFuture.get());
+//        } catch (InterruptedException | ExecutionException e) {
+//            e.printStackTrace();
+//        }
+//        while(!scheduledFuture.isDone());
+//        System.out.println("removing "+scheduledFuture.hashCode());
+//        callableConcurrentHashMap.remove(k);
     }
 
-    public static void main(String... args){
+    public static void main(String... args) throws InterruptedException {
         Callable<Integer> c = () -> {
-            Thread.sleep(1000);
+            Thread.sleep(100);
             System.out.println("calculating........");
             return 1;
         };
         var d = new DayFiftyOne();
-        d.debounce(c, "a", 10);
 
-        d.debounce(c, "b", 10);
-
-        d.debounce(c, "a", 10);
-
+        var start = System.currentTimeMillis();
+        while(System.currentTimeMillis() - start<=500){
+            d.debounce(c, "a", 1);
+            Thread.sleep(99);
+        }
         d.scheduledExecutorService.shutdown();
     }
 
